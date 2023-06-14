9b4d11850f42db2cd87109bcddab06659781452e
Michael Puening <mpuening@gmail.com>
2023-06-13 21:28:31 -0400

    Add more tests

--- test-archetypes.sh
+++ test-archetypes.sh
@@ -31,4 +31,69 @@ else
     exit 1
 fi
 
+# Go back up
+cd ../../..
+
+#
+# Empty Project
+#
+mkdir -p target/archetypes
+cd target/archetypes
+rm -rf my-empty-project
+mvn archetype:generate \
+    -Darchetype.interactive=false --batch-mode \
+    -DarchetypeGroupId=io.github.mpuening \
+    -DarchetypeArtifactId=maven-archetypes-empty \
+    -DarchetypeVersion=0.0.1-SNAPSHOT \
+    -DgroupId=org.example.project \
+    -DartifactId=my-empty-project \
+    -Dversion=0.0.1-SNAPSHOT
+
+if [ $? -eq 0 ]; then
+    echo "Empty Project created..."
+else
+    echo "Empty Project failed to create..."
+    exit 1
+fi
+
+cd my-empty-project
+mvn clean package
+if [ $? -eq 0 ]; then
+    echo "Empty Project built properly..."
+else
+    echo "Empty Project failed to build..."
+    exit 1
+fi
+
+#
+# JSP Project
+#
+mkdir -p target/archetypes
+cd target/archetypes
+rm -rf my-jsp-project
+mvn archetype:generate \
+    -Darchetype.interactive=false --batch-mode \
+    -DarchetypeGroupId=io.github.mpuening \
+    -DarchetypeArtifactId=maven-archetypes-jsp-war \
+    -DarchetypeVersion=0.0.1-SNAPSHOT \
+    -DgroupId=org.example.project \
+    -DartifactId=my-jsp-project \
+    -Dversion=0.0.1-SNAPSHOT
+
+if [ $? -eq 0 ]; then
+    echo "JSP Project created..."
+else
+    echo "JSP Project failed to create..."
+    exit 1
+fi
+
+cd my-jsp-project
+mvn clean package
+if [ $? -eq 0 ]; then
+    echo "JSP Project built properly..."
+else
+    echo "JSP Project failed to build..."
+    exit 1
+fi
+
 exit 0
